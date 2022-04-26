b0e0ef654a4c4a7d225ecfa8953b5bbdd8c7cd0f
josh.94 <jeshua.cabanillas@gmail.com>
2022-04-25 21:31:33 -0500

    style: puppet-lint

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,8 @@
 #Puppet that creates a custom HTTP header response.
-exec {'header':
-  command => 'sudo apt-get update -y,
-	      sudo apt-get install nginx -y,
-	      sudo sed -i "s/^server\s{/server {\n\tadd_header X-Served-By $HOSTNAME;/1" /etc/nginx/sites-available/default
-	      sudo service nginx restart',
-  path    => '/usr/bin'
+exec { 'header':
+command => 'sudo apt-get update -y;
+sudo apt-get install nginx -y;
+sudo sed -i "s/^server\s{/server {\n\tadd_header X-Served-By $HOSTNAME;/1" /etc/nginx/sites-available/default
+sudo service nginx restart',
+path    => '/usr/bin'
 }
