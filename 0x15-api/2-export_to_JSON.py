aafdce98fcce0496c19cf67b7fbdc07dabd9318a
josh.94 <jeshua.cabanillas@gmail.com>
2022-05-20 22:24:41 -0500

    update: line 39 wrong indentation

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-'''Script that, using an REST API, for a given employee ID,
-returns information about his/her TODO list progress.'''
+'''Python script to export data in the JSON format.'''
 import json
 import requests
 from sys import argv
@@ -9,7 +8,7 @@ from sys import argv
 def R_API():
     # Validate if argument has index and is an interger
     try:
-        employee_id = int(argv[1])
+        employeeID = int(argv[1])
 
     except ValueError:
         print('Value Error')
@@ -20,24 +19,26 @@ def R_API():
 
     # Python requets get method'''
     todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                        .format(employee_id))
+                        .format(employeeID))
     user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(employee_id))
+                        .format(employeeID))
     user_name = user.json().get('username')
+    
     # Getting variables
-    data = {}
-    data[employee_id] = []
+    datafile = {}
+    datafile[employeeID] = []
 
-    with open("{}.json".format(employee_id), 'w') as jsonfile:
-        for data_f in todo.json():
-            task_completed_s = data_f.get('completed')
-            task_title = data_f.get('title')
-            data[employee_id].append({
+    # Export data in JSON format
+    with open("{}.json".format(employeeID), "w") as jsonfile:
+        for data in todo.json():
+            task_title = data.get("title")
+            task_completed_s = data.get("completed")
+            datafile[employeeID].append({
                 "task": task_title,
                 "completed": task_completed_s,
                 "username": user_name,
             })
-            json.dump(data, jsonfile)
+        json.dump(datafile, jsonfile)
 
 
 if __name__ == "__main__":
