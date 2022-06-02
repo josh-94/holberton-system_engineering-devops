0badfad00a344bb8e0469e2aa8356bc45802d4d9
josh.94 <jeshua.cabanillas@gmail.com>
2022-06-02 12:01:45 -0500

    update: puppet style

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #Using Puupet to debugg server.
 exec {'sed':
-    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
     provider =>  shell,
 }
