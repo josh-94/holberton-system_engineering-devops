85225b20b7496547e55855cf327aa74817edcc36
josh.94 <jeshua.cabanillas@gmail.com>
2022-06-08 11:52:12 -0500

    Add: task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+#Using Puupet to debugg nginx server by increasing the ULIMIT which handle the amount of requests and responses.
+exec {'sed':
+    command  => 'sed -i "s/15/500/g" /etc/default/nginx',
+    provider =>  shell,
+}
