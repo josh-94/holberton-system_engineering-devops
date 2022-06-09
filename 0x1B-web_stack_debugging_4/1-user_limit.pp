8a24af88c0621892a29aefbd8fedbed0513bc7b3
josh.94 <jeshua.cabanillas@gmail.com>
2022-06-09 12:26:21 -0500

    Update: correcting output code 5

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 #Using Puppet to Change the OS configuration so that it is possible to login with the holberton user opening bigger limit files.
 exec {'sed':
-    command  => 'sed -i "s/holberton soft nofile.*/holberton soft nofile 1000 /"  /etc/security/limits.conf',
+    command  => 'sed -i "s/holberton soft nofile.*/holberton soft nofile 9000 /"  /etc/security/limits.conf',
     provider =>  shell
 }
 exec {'sed':
-    command  => 'sed -i "s/holberton hard nofile.*/holberton hard nofile 1000/ /etc/security/limits.conf',
+    command  => 'sed -i "s/holberton hard nofile.*/holberton hard nofile 9000/ /etc/security/limits.conf',
     provider => shell
 }
