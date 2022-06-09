567a7799413a0b57dd90318a114bcc65da7a9c8d
josh.94 <jeshua.cabanillas@gmail.com>
2022-06-09 12:17:07 -0500

    Update: correcting output code

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,6 @@
 #Using Puppet to Change the OS configuration so that it is possible to login with the holberton user opening bigger limit files.
 exec {'sed':
-    command  => 'sed -e "s/5/15/g" -e "s/4/14/g" /etc/security/limits.conf',
+    command  => 'sed -i "s/5/15/g  /etc/security/limits.conf',
+    command => 'sed "s/4/14/g" /etc/security/limits.conf',
     provider =>  shell,
 }
